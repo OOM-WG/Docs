1bff4eb6e1a9fc61aab27b5ae792b393539b9841
白彩恋 <shiro@oom-wg.dev>
2025-12-18 12:15:20 +0800

    Create html.js

---
+++ content/html.js
@@ -0,0 +1,5 @@
+;(function () {
+	const path = window.location.pathname
+	if (path.endsWith('.html'))
+		window.location.replace(path.slice(0, -5) + window.location.search + window.location.hash)
+})()
