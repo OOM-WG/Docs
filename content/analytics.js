a74b7d09f6bad1bfd78437d21f0bb0e33d6f2489
白彩恋 <shiro@oom-wg.dev>
2026-01-20 18:15:55 +0800

    Update analytics.js

--- content/analytics.js
+++ content/analytics.js
@@ -1,18 +1,5 @@
 ;(function () {
 	if (!['oom-wg.dev', 'oom.mintlify.app'].includes(window.location.hostname)) return
-
-	var appKeys = [
-		{path: '/ssu', key: 'v4cd2cjejx'},
-		{path: '/suu', key: 'v4ciy9mif7'}
-	]
-	var appKey = null
-
-	for (const config of appKeys)
-		if (window.location.pathname.startsWith(config.path)) {
-			appKey = config.path
-			break
-		}
-	if (!appKey) return
 	;(function (c, l, a, r, i, t, y) {
 		c[a] =
 			c[a] ||
@@ -24,11 +11,13 @@
 		t.src = 'https://www.clarity.ms/tag/' + i
 		y = l.getElementsByTagName(r)[0]
 		y.parentNode.insertBefore(t, y)
-	})(window, document, 'clarity', 'script', appKey)
+	})(window, document, 'clarity', 'script', 'v4cpxdpw7p')
+	// Clarity: https://clarity.microsoft.com/
 
 	const script = document.createElement('script')
 	script.src = 'https://static.cloudflareinsights.com/beacon.min.js'
 	script.defer = true
 	script.setAttribute('data-cf-beacon', `{"token": "39151721f49d4b68be95c02bdc214799"}`)
 	document.head.appendChild(script)
+	// Cloudflare Web Analytics: https://www.cloudflare.com/web-analytics/
 })()
