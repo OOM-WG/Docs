15a121b2e529bf461ba3959f578827c696d0cc3e
白彩恋 <shiro@oom-wg.dev>
2026-01-20 18:36:07 +0800

    Update analytics.js

--- content/analytics.js
+++ content/analytics.js
@@ -1,5 +1,12 @@
 ;(function () {
 	if (!['oom-wg.dev', 'oom.mintlify.app'].includes(window.location.hostname)) return
+	function onerror() {
+		console.error(
+			'%c ERROR!!! ',
+			'color: white; background: red; font-size: 88px; font-weight: bold; padding: 22px;'
+		)
+	}
+
 	;(function (c, l, a, r, i, t, y) {
 		c[a] =
 			c[a] ||
@@ -9,6 +16,7 @@
 		t = l.createElement(r)
 		t.async = 1
 		t.src = 'https://www.clarity.ms/tag/' + i
+		t.onerror = onerror
 		y = l.getElementsByTagName(r)[0]
 		y.parentNode.insertBefore(t, y)
 	})(window, document, 'clarity', 'script', 'v4cpxdpw7p')
@@ -18,6 +26,7 @@
 	script.src = 'https://static.cloudflareinsights.com/beacon.min.js'
 	script.defer = true
 	script.setAttribute('data-cf-beacon', `{"token": "39151721f49d4b68be95c02bdc214799"}`)
+	script.onerror = onerror
 	document.head.appendChild(script)
 	// Cloudflare Web Analytics: https://www.cloudflare.com/web-analytics/
 })()
