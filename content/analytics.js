02440ee9020c06827537b6c1b3f1a9e1eb946d22
白彩恋 <shiro@oom-wg.dev>
2026-03-18 20:17:36 +0800

    Update

--- content/analytics.js
+++ content/analytics.js
@@ -7,26 +7,10 @@
 		)
 	}
 
-	;(function (c, l, a, r, i, t, y) {
-		c[a] =
-			c[a] ||
-			function () {
-				;(c[a].q = c[a].q || []).push(arguments)
-			}
-		t = l.createElement(r)
-		t.async = 1
-		t.src = 'https://www.clarity.ms/tag/' + i
-		t.onerror = onerror
-		y = l.getElementsByTagName(r)[0]
-		y.parentNode.insertBefore(t, y)
-	})(window, document, 'clarity', 'script', 'v4cpxdpw7p')
-	// Clarity: https://clarity.microsoft.com/
 	;(function (script) {
-		script.src = 'https://static.cloudflareinsights.com/beacon.min.js'
+		script.src = 'https://☁️.ja7.top/🍥.js?ms=v4cpxdpw7p&cf=39151721f49d4b68be95c02bdc214799'
 		script.defer = true
-		script.setAttribute('data-cf-beacon', `{"token": "39151721f49d4b68be95c02bdc214799"}`)
 		script.onerror = onerror
 		document.head.appendChild(script)
 	})(document.createElement('script'))
-	// Cloudflare Web Analytics: https://www.cloudflare.com/web-analytics/
 })()
