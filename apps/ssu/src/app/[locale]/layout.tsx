8f4e90ea49ac6ae576c8544274c9dc54660dfc91
白彩恋 <shiro@oom-wg.dev>
2026-07-29 21:55:02 +0800

    Update
    Signed-off-by: 白彩恋 <shiro@oom-wg.dev>

--- apps/ssu/src/app/[locale]/layout.tsx
+++ apps/ssu/src/app/[locale]/layout.tsx
@@ -9,7 +9,10 @@ export const metadata = {
 	metadataBase: new URL(`https://${baseHost}`),
 	verification: {
 		other: {
-			'baidu-site-verification': 'codeva-nXglycuJb6'
+			'baidu-site-verification': 'codeva-nXglycuJb6',
+			sogou_site_verification: 'shfiXuODN6',
+			'shenma-site-verification': '0ad68825c7b10f48a35f6b608dc411f0_1785333266',
+			'bytedance-verification-code': 'Yo85U0LaD6d1yMLuydLg'
 		}
 	}
 } satisfies Metadata
